f6c0d3c1571c52db8be2cf68e36e798a54619a5f
suhailroushan13 <suhailroushan13@gmail.com>
2022-03-23 21:05:10 +0530

    Added app2.js ✅

--- app.js
+++ app.js
@@ -5,18 +5,36 @@ import fs from 'fs'
 const httpPort = 80, httpsPort = 443
 
 const httpsServerOptions = {
-    'key': fs.readFileSync('./https/key.pem'),
+    'key': fs.readFileSync('./https/privkey.pem'),
     'cert': fs.readFileSync('./https/cert.pem')
 }
 
 // Creating http server
 let httpServer = http.createServer((req, res) => {
-    res.end(`<h1>hey there from HTTP Mai Http se hu</h1>`)
+    res.end(`<h1>HTTP <br>Port No : 80</h1>   <style>
+body {
+  background-color: linen;
+}
+
+h1 {
+  color: maroon;
+  margin-left: 40px;
+}
+</style>`);
 })
 
 // Creating https server
 let httpsServer = https.createServer(httpsServerOptions,(req, res) => {
-    res.end(`<h1>hey there from HTTPS..Mai Secure hu  </h1>`)
+    res.end(`<h1>HTTPS <br>Port No : 443</h1>   <style>
+body {
+  background-color: white;
+}
+
+h1 {
+  color: green;
+  margin-left: 40px;
+}
+</style>`)
 })
 
 // Listening server at HTTPS port
